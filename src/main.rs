0c06c2ceaf6e227ecdf088acfe1a490cb4cf6f3b
Jack Weber <jackweber142@gmail.com>
2023-02-11 18:30:26 -0500

    implented ray tracing

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,48 @@
 use std::fs::{OpenOptions};
 use std::io::{Write};
 use indicatif::{ProgressBar};
-pub mod color;
+mod color;
 use color::{Color, WriteColor};
-mod point3;
+mod vec3;
+use vec3::{Vec3,UnitVector};
+use vec3::ray::{Ray, GetDirection};
+
+// Source Material
+// https://raytracing.github.io/
+
+// PPM requirements and a PPM Viewer
+// https://web.cse.ohio-state.edu/~shen.94/681/Site/ppm_help.html
+// https://www.cs.rhodes.edu/welshc/COMP141_F16/ppmReader.html
+
+
+fn ray_color(ray: Ray) -> Color{
+    let unit_direction = ray.direction().unit_vector();
+    let t = 0.5*(unit_direction.y + 1.0);
+    return Color{r: 1.0, b: 1.0, g: 1.0}*(1.0-t) + (Color{r:0.5, g: 0.7, b: 1.0}*t);
+
+}
 fn main() {
     
     // Image Properties
-    let image_height = 256;
-    let image_width = 256;
-    let max_ccv = 255;
 
-    // PPM requirements and a PPM Viewer
-    // https://web.cse.ohio-state.edu/~shen.94/681/Site/ppm_help.html
-    // https://www.cs.rhodes.edu/welshc/COMP141_F16/ppmReader.html
+    let aspect_ratio = 16.0/9.0;
+    let image_width = 256;
+    let image_height = (image_width as f64/aspect_ratio) as i32;
+
+    // Camera
+
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio*viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Vec3{x: 0.0, y: 0.0, z: 0.0};
+    let horizontal = Vec3{x:viewport_width, y:0.0, z:0.0};
+    let vertical = Vec3{x: 0.0, y: viewport_height, z:0.0};
+    let lower_left_corner = origin - horizontal/(2.0 as f64) - vertical/(2.0 as f64) - Vec3{x:0.0,y: 0.0,z: focal_length};
+
 
     // Write file and the required ppm header
+
     let file_name = "image.ppm";
     let mut file = OpenOptions::new()
         .read(true)
@@ -24,18 +51,22 @@ fn main() {
         .append(false)
         .open(file_name).unwrap();
 
-    match writeln!(file, "{}", format!("P3 {} {} {}\n", image_width, image_height, max_ccv)) {
+    match writeln!(file, "{}", format!("P3 {} {} {}\n", image_width, image_height, 255)) {
         Ok(_) => (),
         Err(_) => println!("Problem writing header in {:?}", file),
     }
 
-    // Write file contents according to ppm specifications
-    let bar = ProgressBar::new(image_height);
+    // Render
+
+    let bar = ProgressBar::new(image_height as u64);
     bar.set_message("Scanlines");
-    for j in 0..=image_height-1 {
+    for j in (0..=image_height-1).rev() {
         for i in 0..=image_width-1 {
-                let color = Color{r: ((i as f32/image_width as f32) * max_ccv as f32) as u8 , g: ((j as f32/image_height as f32) * max_ccv as f32) as u8, b: 127};
-                color.write_color(&mut file);
+                let u = (i as f64) / ((image_width-1) as f64);
+                let v = (j as f64) / ((image_height-1) as f64);
+                let r = Ray{origin: origin, direction: lower_left_corner + horizontal*u + vertical*v - origin};
+                let color_pixel = ray_color(r);
+                color_pixel.write_color(&mut file);
         }
         bar.inc(1);
     }
